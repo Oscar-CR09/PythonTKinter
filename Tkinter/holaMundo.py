3603ae4e7cad5a0ae5cf329e39f5c174aabb0862
oscar cervantes <oscarcr189@outlook.com>
2022-12-09 23:01:55 -0600

    configuracion

--- Tkinter/holaMundo.py
+++ Tkinter/holaMundo.py
@@ -2,7 +2,8 @@
 #Tkinter - Tk interface
 #importar le modulo de Tkinter
 import tkinter as tk
-
+#importamos el modulo del tema tkinter
+from tkinter import ttk
 #creamos un objeto usando la clase Tk
 ventana = tk.Tk()
 
@@ -10,6 +11,15 @@ ventana = tk.Tk()
 ventana.geometry('600x400')
 #cambiamos el nombre de la ventana
 ventana.title('Hola mundo')
+#configura el icono de la aplicacion debe de estar en la raiz o 
+#o indicar la ruta 
+ventana.iconbitmap('logo.ico')
+
+#creamos un boton (widget), el objeto padre es ventana
+boton1 = ttk.Button(ventana, text='Dar clik')
+#utilizar el pack layout manager para mostrar el boton de la ventana
+boton1.pack()
+
 # Iniciamos la ventana (esta linea la ejecutamos al final )
 # Si la ejecutamos antes, no se muestra los cambios anteriores
 ventana.mainloop()
