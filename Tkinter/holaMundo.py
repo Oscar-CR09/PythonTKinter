93b4f3a9d6f2157c07b96be20964d4b99f4c5641
oscar cervantes <oscarcr189@outlook.com>
2022-12-10 22:59:30 -0600

    actualizadobotones

--- Tkinter/holaMundo.py
+++ Tkinter/holaMundo.py
@@ -13,10 +13,19 @@ ventana.geometry('600x400')
 ventana.title('Hola mundo')
 #configura el icono de la aplicacion debe de estar en la raiz o 
 #o indicar la ruta 
-ventana.iconbitmap('logo.ico')
+ventana.iconbitmap('Tkinter/logo.ico')
+#crear este boton definimos el metodo click
+
+def evento_click():
+    boton1.config(text='Boton presionado')
+    print('ejecutamos del evento_click')
+    #creamos otro boton 
+    boton2= ttk.Button(ventana, text='nuevo boton ')
+    boton2.pack()
+
 
 #creamos un boton (widget), el objeto padre es ventana
-boton1 = ttk.Button(ventana, text='Dar clik')
+boton1 = ttk.Button(ventana, text='Dar clik', command=evento_click)
 #utilizar el pack layout manager para mostrar el boton de la ventana
 boton1.pack()
 
